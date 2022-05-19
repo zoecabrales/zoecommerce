c73ff3769bc94ca245c679c4aa42e2ab03f7e701
Jireh Zoe Dadulla Cabrales <jirehzoedadullacabrales@Jirehs-MacBook-Pro.local>
2022-05-20 00:06:26 +0800

    created authentication page component alongside sign in form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -40,7 +40,7 @@ const SignUpForm = () => {
             await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
         } catch (error) {
-            if(error.code === 'auth/email already in use') {
+            if(error.code = "auth/email-already-in-use") {
                 alert('Cannot create user, email already in use!');
             } else {
                 console.log('user creation encountered an error' , error);
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore, 
@@ -69,4 +70,10 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword (auth, email, password)
+  }
+
+  export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword (auth, email, password)
   }
