1ac16202e1a22b881fd81bdf5e842b1107034b71
zoecabrales <zoe@old.st>
2022-05-25 11:42:01 +0800

    added cart context and shopping icon as weell as cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,20 @@
 import { Outlet, Link } from "react-router-dom"
 import { Fragment, useContext } from "react";
 
-import { ReactComponent as ZoeLogo } from '../../assets/crown.svg' 
-import { UserContext } from "../../contexts/user.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+
+import { ReactComponent as ZoeLogo } from '../../assets/crown.svg'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
     <Fragment>
@@ -31,7 +36,9 @@ const Navigation = () => {
                                 SIGN IN
                             </Link>
                         )}
+                        <CartIcon />
             </div>
+            {isCartOpen && <CartDropdown />}
       </div>
         <Outlet />
     </Fragment>
