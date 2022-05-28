e975abb16dd6f95db905c5ac3bb4916f180d2376
zoecabrales <zoe@old.st>
2022-05-29 02:38:14 +0800

    updated categories context

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,7 +13,11 @@ import {
     getFirestore, 
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs
 } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -40,6 +44,34 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+      const collectionRef = collection(db, collectionKey);
+      const batch = writeBatch(db);
+
+
+      objectsToAdd.forEach((object) => {
+          const docRef = doc(collectionRef, object.title.toLowerCase());
+          batch.set(docRef, object);
+      });
+
+      await batch.commit();
+      console.log('done');
+  };
+
+  export const getCategoriesAndDocuments = async () => {
+      const collectionRef = collection(db, 'categories');
+      const que = query(collectionRef);
+
+      const querySnapshot = await getDocs(que);
+      const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+          const { title, items } = docSnapshot.data();
+          acc[title.toLowerCase()] = items;
+          return acc;
+      }, {})
+
+      return categoryMap;
+  }
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
       if(!userAuth) return;
