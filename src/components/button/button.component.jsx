f7377f929c481cb0075237b15eba7defc8bd5cfa
zoecabrales <zoe@old.st>
2022-06-01 00:03:36 +0800

    integrated styled components to avoid style clashing

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,18 +1,28 @@
-import './button.styles.scss'
+import { BaseButton, GoogleSignInButton, InvertedButton } from './button.styles';
 
-const BUTTON_TYPES_CLASSES = {
+export const BUTTON_TYPES_CLASSES = {
+    base: 'base',
     google: 'google-sign-in',
     inverted: 'inverted',
 };
 
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) => (
+    {
+        [BUTTON_TYPES_CLASSES.base]: BaseButton,
+        [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
+        [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
+        
+    }[buttonType] 
+) 
+
 const Button = ({ children, buttonType, ...otherProps }) => {
+    const CustomButton = getButton(buttonType);
     return (
-        <button 
-            className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} 
+        <CustomButton 
             {...otherProps}
             >
                 {children}
-        </button>
+        </CustomButton>
     );
 };
 
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
     signInAuthUserWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils'
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPES_CLASSES } from '../button/button.component';
 import FormInput from '../form-input/form-input.component'
 
 import './sign-in-form.styles.scss'
@@ -86,7 +86,7 @@ const SignInForm = () => {
 
                 <div className='buttons-container'>
                     <Button type="submit">Sign In</Button>
-                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
+                    <Button type='button' buttonType={BUTTON_TYPES_CLASSES.google} onClick={signInWithGoogle}>Google sign in</Button>
                 </div>
             </form>
         </div>
